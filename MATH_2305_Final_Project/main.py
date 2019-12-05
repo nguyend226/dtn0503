71ee3f8c8f01ba786bd4702a955526cbb555b31d
Dat Nguyen <57651102+nguyend226@users.noreply.github.com>
2019-12-05 15:27:17 -0600

    Update main.py

--- MATH_2305_Final_Project/main.py
+++ MATH_2305_Final_Project/main.py
@@ -1,12 +1,12 @@
 from algorithms import *
-
+from functions.graph_operations import graph_cost
 
 text = input('Please provide a graph  to run the TSP on ')
 text = input('Please provide a starting vertex ')
 
 
 T = prims(G, v)
-print('Optimal Tree is ')
+print(f'Optimal Tree is {T}')
 
 print('')
 
